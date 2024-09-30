53f04bed150c94bd48815827a28ba5c2ff4cdc78
baereva000 <153528325+baereva000@users.noreply.github.com>
2024-09-30 13:21:44 -0600

    Create sorting.py
    bubble sort

---
+++ sorting.py
@@ -0,0 +1,11 @@
+
+def bubble(list)
+  i = 0
+  for i in range(len(list)):
+    j = i + 1
+    for j in range(len(list)):
+      if list[i] < list[j]:
+        temp = list[j]
+        list[j] = list[i]
+        list[i] = temp
+    j = 0
