528dbbf0b13c3d434566200adde514b59f7f34c0
baereva000 <153528325+baereva000@users.noreply.github.com>
2024-09-30 13:58:49 -0600

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -1,12 +1,9 @@
-
 def bubble(list):
-  i = 0
-  for i in range(len(list) - 1):
-    j = i + 1
-    for j in range(len(list)):
-      if list[i] < list[j]:
-        temp = list[j]
-        list[j] = list[i]
-        list[i] = temp
-    j = 0
-return list
+    for i in range(len(list) - 1):
+        for sort in range(len(list) - 1):
+            if list[sort] > list[sort + 1]:
+                temp = list[sort]
+                list[sort] = list[sort + 1]
+                list[sort + 1] = temp
+        sort = 0
+    return list
